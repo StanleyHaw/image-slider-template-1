2102096912cc340022fc7d137713dbf56aec45a8
AlbertNotAlien <albert830207@gmail.com>
2023-08-16 00:38:36 +0800

    refactor: Adjusted the behavior of each function

--- script.js
+++ script.js
@@ -2,82 +2,82 @@ const wrapper = document.querySelector('.wrapper');
 const carousel = document.querySelector('.carousel');
 const leftArrow = document.querySelector('.prev-btn');
 const rightArrow = document.querySelector('.next-btn');
-const image = document.querySelectorAll('img');
+const images = document.querySelectorAll('img');
 const pageButtons = document.querySelectorAll('.page-btn');
-const INTERVAL_MILLISECONDS = 2000;
 
 let imageIndex = 0;
 let intervalId = null;
+
+const INTERVAL_MILLISECONDS = 2000;
+const FIRST_PAGE_INDEX = 0;
+const LAST_PAGE_INDEX = images.length - 1;
 
 const changeImagePage = () => {
   carousel.style.transform = `translate(-${imageIndex * 100}%)`;
 };
 
-const slideImage = () => {
-  if (imageIndex === image.length) {
-    imageIndex = 0;
+const toggleActiveDotStyle = () => {
+  // for (let i = 0; i < images.length; i++) {
+  //   if (i === imageIndex) {
+  //     pageButtons[i].classList.add('page-btn-active');
+  //   } else {
+  //     pageButtons[i].classList.remove('page-btn-active');
+  //   }
+  // }
+
+  pageButtons.forEach((button, index) => {
+    if (index === imageIndex) {
+      button.classList.add('page-btn-active');
+    } else {
+      button.classList.remove('page-btn-active');
+    }
+  });
+};
+
+const increaseActiveImage = () => {
+  if (imageIndex === LAST_PAGE_INDEX) {
+    imageIndex = FIRST_PAGE_INDEX;
   } else {
-    imageIndex;
+    imageIndex++;
   }
+  toggleActiveDotStyle();
   changeImagePage();
 };
 
-const slideDots = () => {
-  for (let i = 0; i < image.length; i++) {
-    pageButtons[i].className = pageButtons[i].className.replace(
-      ' page-btn-active',
-      ''
-    );
-  }
-  pageButtons[imageIndex].className =
-    pageButtons[imageIndex].className + ' page-btn-active';
-};
-
-const autoSlide = () => {
-  intervalId = setInterval(() => {
-    imageIndex++;
-    slideImage();
-    slideDots();
-  }, INTERVAL_MILLISECONDS);
-};
-
-const pauseAutoSlide = () => {
-  clearInterval(intervalId);
-};
-
-const firstPageIndex = 0;
-const lastPageIndex = image.length - 1;
-
-const increaseImageIndex = () => {
-  if (imageIndex === lastPageIndex) {
-    imageIndex = firstPageIndex;
-  } else {
-    imageIndex++;
-  }
-  slideImage();
-  slideDots();
-};
-
-const decreaseImageIndex = () => {
-  if (imageIndex === firstPageIndex) {
-    imageIndex = lastPageIndex;
+const decreaseActiveImage = () => {
+  if (imageIndex === FIRST_PAGE_INDEX) {
+    imageIndex = LAST_PAGE_INDEX;
   } else {
     imageIndex--;
   }
-  slideImage();
-  slideDots();
+  toggleActiveDotStyle();
+  changeImagePage();
+};
+
+const switchActiveImage = (activeDot) => {
+  imageIndex = activeDot;
+  toggleActiveDotStyle();
+  changeImagePage();
+};
+
+const autoSlideImage = () => {
+  intervalId = setInterval(() => {
+    increaseActiveImage();
+  }, INTERVAL_MILLISECONDS);
+};
+
+const pauseAutoSlideImage = () => {
+  clearInterval(intervalId);
 };
 
 pageButtons.forEach((pageBtn, pageIndex) => {
   pageBtn.addEventListener('click', () => {
-    imageIndex = pageIndex;
-    slideImage();
-    slideDots();
+    switchActiveImage(pageIndex);
   });
 });
 
-autoSlide();
-wrapper.addEventListener('mouseover', () => pauseAutoSlide());
-wrapper.addEventListener('mouseleave', () => autoSlide());
-rightArrow.addEventListener('click', () => increaseImageIndex());
-leftArrow.addEventListener('click', () => decreaseImageIndex());
+wrapper.addEventListener('mouseover', pauseAutoSlideImage);
+wrapper.addEventListener('mouseleave', autoSlideImage);
+leftArrow.addEventListener('click', decreaseActiveImage);
+rightArrow.addEventListener('click', increaseActiveImage);
+autoSlideImage();
